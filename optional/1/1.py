2f69ce105cd9a5bdaaad06b75bac2ecee8592bef
NullDev (Shadow) <chris@nulldev.org>
2024-03-29 05:48:57 +0100

    apparently there is more?

---
+++ optional/1/1.py
@@ -0,0 +1,10 @@
+# Google FooBar OPTIONAL Challenge 1
+# Python 2.7.13
+# Forbidden Imports: bz2, crypt, fcntl, mmap, pwd, pyexpat, select, signal, termios, thread, time, unicodedata, zipimport, zlib
+
+def solution(g):
+    pass
+
+print(solution([[True, True, False, True, False, True, False, True, True, False], [True, True, False, False, False, False, True, True, True, False], [True, True, False, False, False, False, False, False, False, True], [False, True, False, False, False, False, True, True, False, False]])) # 11567
+print(solution([[True, False, True], [False, True, False], [True, False, True]])) # 4
+print(solution([[True, False, True, False, False, True, True, True], [True, False, True, False, False, False, True, False], [True, True, True, False, False, False, True, False], [True, False, True, False, False, False, True, False], [True, False, True, False, False, True, True, True]])) # 254
---
+++ optional/readme.txt
@@ -0,0 +1,1 @@
+I completed all the challenges and our of curiosity typed `request` again and... actually got more challenges.
