506c4913e6ee5ab5383e30a4b3233409f6d11f8e
NullDev (Shadow) <chris@nulldev.org>
2024-03-29 15:31:49 +0100

    ill leave it at that for now

---
+++ optional/4/4.py
@@ -0,0 +1,9 @@
+# Google FooBar OPTIONAL Challenge 4
+# Python 2.7.13
+# Forbidden Imports: bz2, crypt, fcntl, mmap, pwd, pyexpat, select, signal, termios, thread, time, unicodedata, zipimport, zlib
+
+def solution(s):
+    pass
+
+print(solution(5)) # 19
+print(solution(77)) # 4208
